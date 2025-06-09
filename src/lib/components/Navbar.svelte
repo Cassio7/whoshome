4effe8d6b95dc330daf5a13b87f7458017b03fd0
cassio7 <acassieri@gmail.com>
2025-06-09 16:01:21 +0200

    first commit

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Button from './ui/button/button.svelte';
+</script>
+
+<div class="flex items-center justify-between p-4">
+	<div class="text-xl font-bold text-white">WhosHome</div>
+	<div class="flex space-x-4">
+		<Button href="/">Home</Button>
+		<Button href="/family">Family</Button>
+	</div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Navbar from '$lib/components/Navbar.svelte';
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>WhosHome</title>
+	<meta name="description" content="This is where the description goes for SEO" />
+</svelte:head>
+
+<Navbar></Navbar>
+{@render children()}
